12ce296a588ff4a4631232e84e4d6a41d9471979
Kyawkhaing444 <kyawkyawkhaing@ucsm.edu.mm>
2020-06-05 23:46:24 +0630

    react-router-day1

---
+++ src/component/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import Topic from './Topic'
+
+function App() {
+  return (
+    <div>
+      <h1> Hello world</h1>
+      <Home />
+      <About />
+      <Topic />
+    </div>
+  );
+}
+
+export default App
---
+++ src/component/Topic.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function topic(){
+    return <div> topic</div>
+} 
