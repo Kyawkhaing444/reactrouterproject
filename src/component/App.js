0af5edca953a9b53ee8b777bc7d6ae0d3d1a4e94
Kyawkhaing444 <kyawkyawkhaing@ucsm.edu.mm>
2020-06-06 10:40:18 +0630

    react-router-day2

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import Home from './Home'
-import About from './About'
-import Topic from './Topic'
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Topic from "./Topic";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1> Hello world</h1>
-      <Home />
-      <About />
-      <Topic />
-    </div>
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topic">Topic</Link>
+          </li>
+        </ul>
+
+        <hr/>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/topic' component={Topic}/>
+
+
+      </div> 
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- src/component/Topic.js
+++ src/component/Topic.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import {
+    Link,
+    Route
+} from 'react-router-dom'
 
-export default function topic(){
-    return <div> topic</div>
+function Topic(props){
+  
+    return <div>{props.match.params.topicId}</div>
+}
+
+export default function topic({match}){
+    return (
+        <div>
+            <h2>
+                Topic
+            </h2>
+            <ul>
+                <li>
+                    <Link to ={`${match.url}/rendering`}>Rendering with React</Link>
+                </li>
+                <li>
+                    <Link to ={`${match.url}/component`}>Components</Link>
+                </li>
+                <li>
+                    <Link to ={`${match.url}/props-vs-state`}>Trops vs State</Link>
+                </li>
+            </ul>
+            <hr/>
+            <Route path={`${match.path}/:topicId`} component={Topic}/>
+
+            <Route exact path={match.path} render={() => {
+                return <h3>Please select a topic</h3>
+            }} />
+        </div>
+    )
 } 
